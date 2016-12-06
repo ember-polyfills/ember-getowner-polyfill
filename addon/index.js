8e0ec68d3bee238f1e4af145c25c8f7803124a19
Robert Jackson <rjackson@linkedin.com>
2016-12-06 16:40:55 -0500

    Clean up index.js to rely on `Ember.getOwner` polyfill.

--- addon/index.js
+++ addon/index.js
@@ -1,22 +1,7 @@
 import Ember from 'ember';
-import FakeOwner from './fake-owner';
-
-let hasGetOwner = !!Ember.getOwner;
 
 Ember.deprecate("ember-getowner-polyfill is now a true polyfill. Use Ember.getOwner directly instead of importing from ember-getowner-polyfill", false, {
   id: "ember-getowner-polyfill.import"
 });
 
-export default function(object) {
-  let owner;
-
-  if (hasGetOwner) {
-    owner = Ember.getOwner(object);
-  }
-
-  if (!owner && object.container) {
-    owner = new FakeOwner(object);
-  }
-
-  return owner;
-}
+export default Ember.getOwner;
