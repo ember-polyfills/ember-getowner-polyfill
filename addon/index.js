bfaa88c755dfe1935388a868810a6475464138da
Edward Faulkner <ef@alum.mit.edu>
2016-12-07 21:50:58 -0500

    update based on rwjblue comment in PR #11

--- addon/index.js
+++ addon/index.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 Ember.deprecate("ember-getowner-polyfill is now a true polyfill. Use Ember.getOwner directly instead of importing from ember-getowner-polyfill", false, {
   id: "ember-getowner-polyfill.import",
-  until: '3.0.0'
+  until: '2.0.0'
 });
 
 export default Ember.getOwner;
