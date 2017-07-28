8f638791b0ce4cdcfa502ff05a8e07a9be46921d
Robert Jackson <rjackson@linkedin.com>
2017-07-28 10:14:12 -0400

    Remove deprecated module import syntax (in favor of polyfill version).
    Also removes ember-cli-babel dependency.

--- addon/index.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-Ember.deprecate("ember-getowner-polyfill is now a true polyfill. Use Ember.getOwner directly instead of importing from ember-getowner-polyfill", false, {
-  id: "ember-getowner-polyfill.import",
-  until: '2.0.0'
-});
-
-export default Ember.getOwner;
