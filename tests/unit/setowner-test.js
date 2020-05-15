aeb1f672ff2659b9077516c89b4569999520e72c
Martin Muñoz <martin.munoz.temp@addepar.com>
2020-05-15 17:43:58 -0400

    setOwner sets the container in Ember 1.x

--- tests/unit/setowner-test.js
+++ tests/unit/setowner-test.js
@@ -1,8 +1,15 @@
 import Ember from 'ember';
-import { module, test } from 'ember-qunit';
-const { getOwner, setOwner } = Ember;
+import EmberObject from '@ember/object';
+import { getOwner, setOwner } from '@ember/application';
+import { inject as service } from '@ember/service';
+import { moduleFor, test } from 'ember-qunit';
+import { lte } from 'ember-compatibility-helpers'
 
-module('setOwner');
+moduleFor('foo:bar', {
+  afterEach(assert) {
+    assert.noDeprecationsOccurred();
+  }
+});
 
 test('setOwner works with getOwner', function(assert) {
   let object = {};
@@ -14,3 +21,40 @@ test('setOwner works with getOwner', function(assert) {
 
   assert.strictEqual(getOwner(object), owner, 'the owner is set correctly');
 });
+
+if (lte('1.13.13')) {
+  test('[Ember 1.x]: setOwner sets the container if the owner has a container', function(assert) {
+    this.register('service:session', Ember.Object);
+
+    let Foo = EmberObject.extend({
+      session: service()
+    });
+
+    let foo = Foo.create();
+
+    setOwner(foo, getOwner(this));
+
+    assert.ok(!!foo.get('session'), 'session injection exists');
+  });
+
+  test('[Ember 1.x]: setOwner does not crash if the owner does not have a container', function(assert) {
+    this.register('service:session', Ember.Object);
+
+    let Foo = EmberObject.extend({
+      session: service()
+    });
+
+    let owner = {};
+    let foo = Foo.create();
+
+    setOwner(foo, owner);
+
+    assert.throws(() => {
+      foo.get('session');
+    }, /Attempting to lookup an injected property on an object without a container/)
+  });
+
+  test('[Ember 1.x]: setOwner does not crash if the owner is undefined', function() {
+    setOwner({}, undefined);
+  });
+}
